99401eeddee7d69465f97dbd5498b989824967e7
haocp <haocpse184107@fpt.edu.vn>
2025-05-18 13:00:09 +0700

    init eureka

---
+++ eureka-discovery/src/main/java/com/haocp/eureka_discovery/EurekaDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.haocp.eureka_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
+	}
+
+}
