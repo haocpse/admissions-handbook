a05d3d063b26da1c3c5ba79b9591cdf566ded2cf
Uông Đình Dũng <dungminhhao@gmail.com>
2025-06-26 12:31:14 +0700

    fix

--- chat-service/src/main/java/com/dungud/chat_service/ChatServiceApplication.java
+++ chat-service/src/main/java/com/dungud/chat_service/ChatServiceApplication.java
@@ -2,7 +2,7 @@ package com.dungud.chat_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -12,7 +12,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ChatServiceApplication.class, args);
 	}
-
 }
